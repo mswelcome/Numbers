0aebdd83b0b077b617250d2249eab300c3c0b54a
mswelcome <mattwelcome@gmail.com>
2017-06-26 15:16:05 -0400

    Adding user input of integers together

---
+++ add.rb
@@ -0,0 +1,15 @@
+def add1()
+	puts "What is the first number?"
+	x = gets.to_i
+end
+
+def add2()
+	puts "What is the second number?"
+    y = gets.to_i 
+end
+
+def result()
+	puts add1 + add2
+end
+
+puts result
